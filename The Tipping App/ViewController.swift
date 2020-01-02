6d91a509c80121dafb4b716fc0c08c92cfefd278
Azmeh Durrani <akdurrani21@gmail.com>
2020-01-02 11:20:50 -0800

    Initial Commit

---
+++ The Tipping App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Tipping App
+//
+//  Created by Azmeh Durrani on 1/2/20.
+//  Copyright © 2020 Azmeh Durrani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
